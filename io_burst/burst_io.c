bb57c4117e3592a95e7c0e8f240214cfa502ebe0
David Valin <dvalin@redhat.com>
2021-09-28 11:38:28 -0400

    Put license back

--- io_burst/burst_io.c
+++ io_burst/burst_io.c
@@ -1,3 +1,25 @@
+/*
+ *                         License
+ *
+ * Copyright (C) 2021  David Valin dvalin@redhat.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * This program is set up to generate a set of IO activity for a specific period
+ * of time, sleep for so long and repeat.
+ */
 #define _LARGEFILE64_SOURCE 
 #include <stdio.h>
 #include <stdlib.h>
